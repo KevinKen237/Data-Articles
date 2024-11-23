55af3fb82960567aa429cc8d2fe59e41ddd10641
kevin <kevinkenang1@gmail.com>
2024-11-23 01:10:57 +0100

    get html data

---
+++ scrapping_data.py
@@ -0,0 +1,24 @@
+# Dans ce fichier, nous allons scrapper les données et les stocker dans différents fichiers
+
+# Importation des librairies
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
+import random
+import time
+import sys
+
+
+# Fonction pour récupérer le contenu html d'une page web
+def get_bsobj_from_url(mon_url):
+    time.sleep(random.uniform(0.4, 1))
+    # Ouvrir avec openurl mon_url
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    req = Request(mon_url,headers={'User-Agent':user_agent})
+
+    try: # gestion des exceptions avec un bloc try/except
+        html = urlopen(req)
+    except (HTTPError, URLError) as e:
+        sys.exit(e) # sortie du programme avec affichage de l’erreur
+    bsobj = BeautifulSoup(html, "lxml") # en utilisant le parser de lxml
+    return bsobj
